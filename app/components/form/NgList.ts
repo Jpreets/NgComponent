2ea7ca108123524f488c77c6b992484aa5d99207
Anshul Gupta <anshulgupta231193@gmail.com>
2017-02-10 21:24:15 +0530

    Radio Buttons Component Integration.

--- app/components/form/NgList.ts
+++ app/components/form/NgList.ts
@@ -2,34 +2,51 @@
 import {Component,Input,Output,EventEmitter} from "angular2/core"
 
 @Component({
-	selector:'ng-list',
-	template:`
-	<div class="list-group">
-  	  <div 
-		*ngFor="#rec of data" 
-		[ngClass]="rec.disable?
-		'list-group-item list-group-item-'+rec.type+' disabled':
-		'list-group-item list-group-item-'+rec.type +' '+(rec.id==selectedRecord.id?'selected':'') "
-		[innerHTML]="rec.body"
-		 (click)="selectRow(rec)" 
-		 ></div>
-	  </div>
-	`
+    selector:'ng-list',
+    template:`
+    <div class="list-group">
+      <div 
+        *ngFor="#rec of data" 
+                (click)="selectRow(rec)"
+        [ngClass]="rec.disable?
+        'list-group-item list-group-item-'+rec.type+' disabled':
+        'list-group-item list-group-item-'+rec.type +' '+(checkRecord(rec)?'selected':'') "
+        [innerHTML]="rec.body" 
+         ></div>
+      </div>
+    `
 })
 export class NgList{ 
 
-	@Input() data=[];
-	@Input() multiselect = false;
-	@Input() selectable=false;
-	public selectedRecord={id:0} ;
+    @Input() data=[];
+    @Input() multiselect = false;
+    @Input() selectable=false;
+    public selectedRecord=[] ;
 
-  	@Output() onSelectionChange = new EventEmitter();
+    @Output() onSelectionChange = new EventEmitter();
+        
+        checkRecord(rec){
+            for(var i=0;i<this.selectedRecord.length;i++){
+                if(rec.id == this.selectedRecord[i].id){ 
+                    return true;
+                }
+            }
+            return false;
+        }
 
- 	 selectRow(record) {
-		if(!this.selectable) return;
-    
-		this.selectedRecord= record;
-     	this.onSelectionChange.emit(this.selectedRecord);
+     selectRow(record) {
+        if(!this.selectable) return;
+                flag = 0;
+                for(var i=0;i<this.selectedRecord.length;i++){
+                    if(record.id == this.selectedRecord[i].id){
+                        this.selectedRecord.splice(this.selectedRecord.indexOf(record), 1);
+                        flag = 1;
+                    }
+                }
+                if(flag == 0)
+                    this.selectedRecord.push(record);
+
+                this.onSelectionChange.emit(this.selectedRecord);
 
   }
 
